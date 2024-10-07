ea9ba19ccae9a15d12b3f902dc112f4d0c47bd8f
Priya Samanta <121284686+Priya-Samanta@users.noreply.github.com>
2024-10-07 23:58:14 +0530

    third commit

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -5,14 +5,14 @@ import styles from '../styles/Home.module.css'
 
 export default function Home(props) {
   return ( 
-    <section class="text-gray-600 body-font">
-    <div class="container px-5 py-24 mx-auto">
-      <div class="flex flex-wrap w-full mb-20">
-        <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
-          <div class="h-1 w-20 bg-indigo-500 rounded"></div>
+    <section className="text-gray-600 body-font">
+    <div className="container px-5 py-24 mx-auto">
+      <div className="flex flex-wrap w-full mb-20">
+        <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+          <div className="h-1 w-20 bg-indigo-500 rounded"></div>
         </div>
       </div>
-      <div class="flex flex-wrap -m-4">
+      <div className="flex flex-wrap -m-4">
       {props.game.data.map((item, index) => {
           return (
           <div key={index} className="xl:w-1/4 md:w-1/2 p-4">
